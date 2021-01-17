64b5ed9c39e6575f816e00c51aab8fa21e59cff6
Xybaks <nikolai.berestevich@gmail.com>
2021-01-17 22:51:43 +0300

    Updates

---
+++ precache-manifest.66e25888fc76b06ed9d83580263508b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c910227d345b6b3e754b100222254f4a",
+    "url": "/simpleReactHomeworks/index.html"
+  },
+  {
+    "revision": "e30d70721ccb2ab54e61",
+    "url": "/simpleReactHomeworks/static/css/main.d90c0a80.chunk.css"
+  },
+  {
+    "revision": "b3ec382f7bfbabe7fa46",
+    "url": "/simpleReactHomeworks/static/js/2.03b0692f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simpleReactHomeworks/static/js/2.03b0692f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30d70721ccb2ab54e61",
+    "url": "/simpleReactHomeworks/static/js/main.e4f80106.chunk.js"
+  },
+  {
+    "revision": "68e085a00f5b2869fa75",
+    "url": "/simpleReactHomeworks/static/js/runtime-main.1ae8533a.js"
+  }
+]);
