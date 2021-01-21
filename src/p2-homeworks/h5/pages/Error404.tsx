9868655da3c2964d98cf0d1728564eb226f52cc5
Xybaks <nikolai.berestevich@gmail.com>
2021-01-21 10:26:54 +0300

    alert in hw5 deleted

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -4,7 +4,6 @@ import s from "./Error404.module.css";
 function Error404() {
     return (
         <div className={s.errorPage}>
-            {alert ("Error404")}
             <div className={s.title}> Error 404:</div>
             <div className={s.someText}>Page not found! </div>
             <div className={s.someSmile}>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
